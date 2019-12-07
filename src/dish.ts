c611327cda13d6e339da594d9d930593b68f2efe
Wasya1212 <leonardo.1212@yandex.ua>
2019-12-07 16:34:30 +0200

    added dish class

--- src/dish.ts
+++ src/dish.ts
@@ -1,5 +1,28 @@
-import 
+import { Recipe, RecipeIngredient as Ingredient, RecipeDetails } from "./recipe";
 
-export default class Dish {
-
+interface DishDetails extends RecipeDetails {
+  photo?: string,
+  price: number
 }
+
+class Dish extends Recipe {
+  protected photo?: string = "";
+  protected price: number = 0;
+
+  constructor(details: DishDetails) {
+    super(details);
+
+    this.photo = details.photo;
+    this.price = details.price;
+  }
+
+  public showPrice(): number {
+    return this.price;
+  }
+
+  public sell(count: number): number {
+    return count * this.price;
+  }
+}
+
+export { Dish, Ingredient };
